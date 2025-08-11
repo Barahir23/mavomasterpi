6d434bf3d618f20e214016f35f6258746e2f4331
barahir23 <barahir5891@gmx.ch>
2025-08-11 21:22:22 +0200

    v0.0.1

--- mavomaster/asgi.py
+++ mavomaster/asgi.py
@@ -13,7 +13,6 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import counter.routing
 import messung.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mavomaster.settings')
@@ -25,7 +24,7 @@ application = ProtocolTypeRouter({
     # WebSocket-Anfragen werden an unser eigenes Routing weitergeleitet
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            counter.routing.websocket_urlpatterns + messung.routing.websocket_urlpatterns
+            messung.routing.websocket_urlpatterns
         )
     ),
 })
