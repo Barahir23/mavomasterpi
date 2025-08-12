967eaa9e4cbdba58f89c2154588036ba9c7e0099
Barahir <31481615+Barahir23@users.noreply.github.com>
2025-08-12 22:08:39 +0200

    Add measurement delete confirmation template

--- messung/forms.py
+++ messung/forms.py
@@ -1,14 +1,23 @@
 from django import forms
-from .models import Projekt, Objekt
+from .models import Projekt, Objekt, Messdaten
 
 
 class ProjektForm(forms.ModelForm):
     class Meta:
         model = Projekt
         fields = ['code', 'name', 'beschreibung']
+        widgets = {
+            'beschreibung': forms.Textarea(attrs={'rows': 1})
+        }
 
 
 class ObjektForm(forms.ModelForm):
     class Meta:
         model = Objekt
         fields = ['projekt', 'nummer', 'name']
+
+
+class MessungForm(forms.ModelForm):
+    class Meta:
+        model = Messdaten
+        fields = ['anforderung', 'messbedingungen', 'messhoehe']
